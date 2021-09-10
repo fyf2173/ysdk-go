2813fd68f9b497eac966e1044bbe6ab951aa06c0
yunfei.feng <yunfei.feng@tophant.com>
2021-09-10 17:17:16 +0800

    fix

--- util/func.go
+++ util/func.go
@@ -1,6 +1,10 @@
 package util
 
-import "strings"
+import (
+	"net"
+	"strings"
+	"time"
+)
 
 // TrimFloatZeroSuffix 格式化浮点数，去除小数点末尾的0或.
 func TrimFloatZeroSuffix(floatStr string) string {
@@ -27,4 +31,28 @@ func Assert(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func PickUnusedPort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
+	if err != nil {
+		return 0, err
+	}
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	port := l.Addr().(*net.TCPAddr).Port
+	if err := l.Close(); err != nil {
+		return 0, err
+	}
+	return port, nil
+}
+
+func NanotimeToDatetime(nanoTime int64) string {
+	return time.Unix(nanoTime/1e9, 0).Format("2006-01-02 15:04:05")
+}
+
+func TimeFixDate(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
