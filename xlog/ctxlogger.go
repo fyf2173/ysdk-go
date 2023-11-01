9dc7e02df877e668d97138bce410c49d6f306033
fengyunfei <fengyunfei@vchangyi.com>
2023-11-01 18:23:55 +0800

    fix

--- xlog/ctxlogger.go
+++ xlog/ctxlogger.go
@@ -3,9 +3,24 @@ package xlog
 import (
 	"context"
 	"log/slog"
+	"os"
 
 	"github.com/fyf2173/ysdk-go/xctx"
 )
+
+var levelMap = map[string]slog.Level{
+	"debug": slog.LevelDebug,
+	"info":  slog.LevelInfo,
+	"warn":  slog.LevelWarn,
+	"error": slog.LevelError,
+}
+
+func Init(level string, addSource bool) {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level:     levelMap[level],
+		AddSource: addSource,
+	})))
+}
 
 func Debug(ctx context.Context, msg string, args ...slog.Attr) {
 	args = append(args, slog.String("trace_id", xctx.CtxId(ctx)))
@@ -22,7 +37,10 @@ func Warn(ctx context.Context, msg string, args ...slog.Attr) {
 	slog.LogAttrs(ctx, slog.LevelWarn, msg, args...)
 }
 
-func Error(ctx context.Context, msg string, args ...slog.Attr) {
+func Error(ctx context.Context, err error, args ...slog.Attr) {
+	if err == nil {
+		return
+	}
 	args = append(args, slog.String("trace_id", xctx.CtxId(ctx)))
-	slog.LogAttrs(ctx, slog.LevelError, msg, args...)
+	slog.LogAttrs(ctx, slog.LevelError, err.Error(), args...)
 }
